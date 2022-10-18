c64e6e22c7f3ff51221ab4f9ccdfeb8ee1119abb
Rafoevistep <rafovirabyanevistep@gmail.com>
2022-10-18 16:36:26 +0400

    Google search

---
+++ app/Http/Controllers/Api_v1/SearchController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Api_v1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SearchController extends Controller
+{
+    public function search_google(Request $request){
+        $name = $request->search;
+        $search = "https://www.google.com/search?q=" . $name. "&rlz=1C5CHFA_enNZ948NZ948&oq=" .$name . "&aqs=chrome.0.69i59l2j46i175i199i433j46i199i291i433j46j0i433j0j69i60.875j0j9&sourceid=chrome&ie=UTF-8";
+        return redirect($search);
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+Route::get('search', [App\Http\Controllers\Api_v1\SearchController::class, 'search_google'])->name('search_name');
