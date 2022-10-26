3112c182c171008d7ac36dbbe4ae4ad82948cded
Rafoevistep <110041235+Rafoevistep@users.noreply.github.com>
2022-10-26 17:13:32 +0400

    Update SearchController.php

--- app/Http/Controllers/Api_v1/SearchController.php
+++ app/Http/Controllers/Api_v1/SearchController.php
@@ -12,25 +12,19 @@ class SearchController extends Controller
     public function search_google(Request $request): \Illuminate\Http\JsonResponse
     {
         $name = $request->search;
-
-        $client = new GoogleSearchResults('dc86fb9fef9e738dbed8c4a26b25f4a6101f9940b4f61698954b12ac16cbb5d7');
+        $client = new GoogleSearchResults("dc86fb9fef9e738dbed8c4a26b25f4a6101f9940b4f61698954b12ac16cbb5d7");
         $query = ["q" => $name, "location" => "Armenia,Yerevan"];
         $response = $client->get_json($query);
         $result = $response->organic_results;
         //organic_results
         $itemDataList = [];
-
-        foreach ($result as $itemElement) {
-
-            $link = $itemElement->link;
-            $title = $itemElement->title;
-
+        
+        for($k=0; $k < count($result); $k++) {
+            $link = $result[$k]->title;
+            $title = $result[$k]->link;
             $itemDataList[] = ['link' => $link, 'title' => $title];
-
         }
 
         return response()->json($itemDataList);
-
     }
-
 }
